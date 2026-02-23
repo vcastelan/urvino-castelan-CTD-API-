c0bbb19ac50db8ee282d2864d2fe4b0a58734974
Urvino <78988375+vcastelan@users.noreply.github.com>
2026-02-23 13:06:22 -0600

    Created a new fetch request to use an Open source API and console.log that data

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # urvino-castelan-CTD-API-
 API open project for Intro to Programming course with Code the Dream
+[https://www.swapi.tech/api/]
+(https://github.com/vcastelan/urvino-castelan-CTD-API-)
---
+++ js/index.js
@@ -0,0 +1,15 @@
+fetch('https://www.swapi.tech/api/') 
+.then(response => {
+  if (!response.ok) {
+    throw new Error('Request failed: ', response.status);
+  }
+  //parse the return response via JSON
+  return response.json();
+})
+//what we will do with our data
+.then(data => {
+  console.log(data);
+})
+.catch(error => {
+  console.error('An eror occured: ', error);
+});
