beb3712547815be8179c465c8f949ac243e395b5
Urvino <78988375+vcastelan@users.noreply.github.com>
2026-03-07 20:22:08 -0800

    Adjusted my fetch data to fetch each time a new character is selected. Also removed 1 character display and only displays one character at a time. I also set most of my DOM variables as global. As well as changed my event listeners to outside of one of my fetch requests.  Finally reorder my .js file for readability.

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+//  FILMS
 const filmImgs = [
   "Images/01 A New Hope.jpg",
   "Images/02 Empire Strikes Back.jpg",
@@ -7,7 +8,49 @@ const filmImgs = [
   "Images/06 Revenge of the Sith.jpg"
 ];
 
+// CHARACTERS
+//array of character images, grabbed from our images folder
+const characterImages = [
+  "Images/01 Luke.jpeg",
+  "Images/02 C-3PO.webp",
+  "Images/03 R2-D2.avif",
+  "Images/04 Darth Vader.jpg",
+  "Images/05 Leia.jpg",
+  "Images/06 Owen Lars.webp",
+  "Images/07 beru whitesun.webp",
+  "Images/08 R5-d4.avif",
+  "Images/09 Biggs Darklighter.webp",
+  "Images/10 Ben Kenobi.png",
+]
+
+//HOLD current charcter and movie/film
+let currentPerson = 1;
+//keep track of current film
+let filmIndex = 0;
+
+// DOM Manipulation
+//variable to hold our film title
+const currentFilmTitle = document.getElementById('film-title');
+//variable to select our img element
+const filmImg = document.getElementById('movie-img');
+//variable to select our film pargraph element
+const filmSection = document.getElementById('film-info');
+const filmDescription = filmSection.querySelector('pre');
+//variables to hold our next and previous film buttons
+const previousFilm = document.getElementById('previous-film');
+const nextFilm = document.getElementById('next-film')
+//variable to hold character name
+const currentCharacterOne = document.getElementById('character-one-name');
+// variable to hold images of characters
+const characterImgOne = document.getElementById('char-one-img');
+// variable to select our paragraph chararcter description
+const charDescriptionOne = document.getElementById('char-one-description');
+//variable to select our previous and next button elements
+const previousCharacter = document.getElementById('previous-button');
+const nextCharacter = document.getElementById('next-button');
+
 //USING ASYNC + AWAIT
+// FETCH
 const fetchData = async() => {
   //try catch error to handle promise rejections
   try {
@@ -23,32 +66,15 @@ const fetchData = async() => {
     //what we will do with our data
     console.log(data.result);
 
-    // FILM CHANGES
-  
     //start at first film
     const film = data.result;
-    //keep track of current film
-    let filmIndex = 0;
 
-    //variable to hold our film title
-    const currentFilmTitle = document.getElementById('film-title');
-    
-    //variable to select our img element
-    const filmImg = document.getElementById('movie-img');
     //set our current img
     filmImg.src = filmImgs[0];
   
-    //variable to select our film pargraph element
-    const filmSection = document.getElementById('film-info');
-    const filmDescription = filmSection.querySelector('pre');
-
     // set our film title and description
     currentFilmTitle.textContent = film[filmIndex].properties.title;
     filmDescription.textContent = film[filmIndex].properties.opening_crawl;
-
-    //variables to hold our next and previous film buttons
-    const previousFilm = document.getElementById('previous-film');
-    const nextFilm = document.getElementById('next-film')
 
     //event listener to click a button and change next film
     nextFilm.addEventListener('click', event => {
@@ -80,7 +106,7 @@ const fetchData = async() => {
       //increment index by 1 to update our film index
       filmIndex = (filmIndex - 1);
   
-      console.log(filmIndex);
+      // console.log(filmIndex);
     });
 
     //forEach to print to the console what we want. In this case title information for our films
@@ -96,139 +122,73 @@ const fetchData = async() => {
 } 
 fetchData();
 
-//array of character images, grabbed from our images folder
-const characterImages = [
-  "Images/01 Luke.jpeg",
-  "Images/02 C-3PO.webp",
-  "Images/03 R2-D2.avif",
-  "Images/04 Darth Vader.jpg",
-  "Images/05 Leia.jpg",
-  "Images/06 Owen Lars.webp",
-  "Images/07 beru whitesun.webp",
-  "Images/08 R5-d4.avif",
-  "Images/09 Biggs Darklighter.webp",
-  "Images/10 Ben Kenobi.png",
-]
+const fetchDataThree = async(currentPerson) => {
+  const currentUrl = `https://www.swapi.tech/api/people/${currentPerson}`;
 
-const fetchDataTwo = async() => {
+  // charDescriptionOne.textContent = 'Loading Description....PLEASE WAIT BEFORE YOU PRESS NEXT OR PREVIOUS';
+  
   //try catch error to handle promise rejections
   try {
-    //create a variable to await for an asynchronous response to finish.
-    const response = await fetch('https://www.swapi.tech/api/people/');
+    const responseDesc = await fetch(currentUrl);
 
-    const responseDesc = await fetch('https://www.swapi.tech/api/people/1');
-    //variable to hold the response results and converted to json format
-    const people = await response.json();
     const desc = await responseDesc.json();
 
     //if reponse throws an error
-    if (!response.ok) {
+    if (!responseDesc.ok) {
       throw new Error('Request failed: ', response.status);
     }
-    //what we will do with our data
-    console.log(people.results);
+    console.log(desc.result.properties);
 
-    //forEach to print to the console what we want. In this case title information for our films
-    // people.results.forEach((person) => {
-    //   console.log(person.name);
-    // });
-    // console.log(people.results[0].name);
-    //or 
+    characterImgOne.src = characterImages[currentPerson - 1];
 
-    // CHARACTER CHANGES
-    //variable to hold current character
-    const characters = people.results;
+    //variable to hold our response json 
+    const characterOneInfo = desc.result.properties;
 
-    //checking to see if i can get description of character
-    // console.log(people.results[0].url);
-    // console.log(desc.result.description);
+    //display our character name in our h3 title
+    currentCharacterOne.innerText = characterOneInfo.name;
 
-    //start at first character
-    let index = 0;
+    //diplay our character description content
+    charDescriptionOne.textContent =
+    `Height: ${characterOneInfo.height}
+    | Mass: ${characterOneInfo.mass}
+    | Gender: ${characterOneInfo.gender}
+    | Birth Year: ${characterOneInfo.birth_year}`;
 
-    //variables to hold character names
-    const currentCharacterOne = document.getElementById('character-one-name');
-    const currentCharacterTwo = document.getElementById('character-two-name')
-
-    //variable to hold previous and next buttons to change characters
-    const previousCharacter = document.getElementById('previous-button');
-    const nextCharacter = document.getElementById('next-button')
-
-    // variables to hold images of characters
-    const characterImgOne = document.getElementById('char-one-img');
-    const characterImgTwo = document.getElementById('char-two-img');
-
-    //variable to hold description of characters
-    const charDescriptionOne = document.getElementById('char-one-description');
-    const charDescriptionTwo = document.getElementById('char-two-description');
-
-    // current characters names, imgs and descriptions
-    currentCharacterOne.innerText = characters[index].name;
-    currentCharacterTwo.innerText = characters[index+1].name;
-    characterImgOne.src = characterImages[0];
-    characterImgTwo.src = characterImages[1];
-    charDescriptionOne.textContent = characters[index].description;
-    charDescriptionTwo.textContent = characters[index+1].description;
-
-    // Function to change the character on click
-    //event listener to click a button and change character
-    nextCharacter.addEventListener('click', event => {
-      //if character length is greater than amount we have reset the index value to beginning.
-      if (index >= 8) {
-        index = -2;
-      } 
-    
-      //change characters title
-      currentCharacterOne.innerText = characters[index+2].name;
-      currentCharacterTwo.innerText = characters[index+3].name;
-
-      //change characters image
-      characterImgOne.src = characterImages[index+2];
-      characterImgTwo.src = characterImages[index+3];
-
-      //change characters description
-
-      //increment index by 1 to update our character index
-      index = (index + 1);
-      index = (index + 1);
-
-      // console.log(index);
-    });
-
-    previousCharacter.addEventListener('click', event => {
-      if (index <= 0) {
-        index = 10;
-      } 
-      //change characters title
-      currentCharacterOne.innerText = characters[index-2].name;
-      currentCharacterTwo.innerText = characters[index-1].name;
-    
-      //change characters image
-      characterImgOne.src = characterImages[index-2];
-      characterImgTwo.src = characterImages[index-1];
-
-       //change characters description
-    
-      //increment index by 1 to update our character index
-      index = (index - 1);
-      index = (index - 1);
-    
-      // console.log(index);
-
-    });
-
-    // //for loop to check if character length is greater than current length we are at
-    for (let i = 0; i < people.results.length; i++) {
-      console.log(people.results[i].name);
-    }
-
-  //to reponse to errors in the try statement
+    //to reponse to errors in the try statement
   } catch(error) {
     console.error('An eror occured: ', error);
   }
 }
 
-fetchDataTwo();
+// CLICK EVENT LISTENERS
+nextCharacter.addEventListener('click', () => {
+  //if character length is greater than amount we have reset the index value to beginning.
+  if (currentPerson >= 10) {
+    currentPerson = 1;
+  } else {
+    currentPerson ++;
+  }
+
+  //change characters description
+  fetchDataThree(currentPerson);
+
+  // console.log(currentPerson);
+});
+
+previousCharacter.addEventListener('click', () => {
+  if (currentPerson <= 1) {
+    currentPerson = 10;
+  } else {
+    currentPerson --;
+  }
+
+  //change characters description
+  fetchDataThree(currentPerson);
+  // console.log(currentPerson);
+
+});
+
+fetchDataThree(currentPerson);
 
 //USING PROMISE
 
